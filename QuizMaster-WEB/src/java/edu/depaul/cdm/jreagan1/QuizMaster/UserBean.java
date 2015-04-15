8b429cce8f3e046df6a14f31f5387f8b1f117542
John Reagan <jreagan@bestcase.com>
2015-04-15 18:46:10 -0500

    Initial commit. Userlist

---
+++ QuizMaster-WEB/src/java/edu/depaul/cdm/jreagan1/QuizMaster/UserBean.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.depaul.cdm.jreagan1.QuizMaster;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author John
+ */
+@SessionScoped
+@Named
+public class UserBean implements Serializable {
+    private static final Logger logger = Logger.getLogger(UserBean.class.getName());
+    
+    @PersistenceContext(unitName = "QuizMaster-WEBPU")
+    private EntityManager entityManager;
+     
+    public List<User> getUserList() throws SQLException {
+        logger.info("Before getting connection");
+        List<User> list = new ArrayList<User>();
+//        EntityManager em = this.entityManager;
+//        logger.info("EM = " + em.toString());
+//        logger.info("C = " + em.find(User.class, 1));
+//        Query nm = em.createNamedQuery("getAllUsers", User.class);
+//        List<User> list = nm.getResultList();
+        list = entityManager.createNamedQuery("findAllUsers").getResultList();
+        logger.log(Level.INFO, "Before returning: {0}", list.size());
+        
+        return list;
+    }
+}
---
+++ QuizMaster-WEB/src/main/resources/META-INF/load.sql
@@ -0,0 +1,3 @@
+insert into USERS(id, USERNAME, PASSWORD) values (1, 'To Kill a MockingBird', 'PW')
+insert into USERS(id, USERNAME, PASSWORD) values (2, 'James Bond', 'PW')
+insert into USERS(id, USERNAME, PASSWORD) values (3, 'Intro to JEE7', 'PW')
