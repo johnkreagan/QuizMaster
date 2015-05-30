317fdb6926204723c204e690c9cff2236468f61c
John Reagan <jreagan@bestcase.com>
2015-05-30 14:35:58 -0500

    Added second pub/sub bean

--- maven/QuizMaster/QuizMaster-ejb/src/main/java/edu/depaul/cdm/QuizMaster/mdb/QuizMatchCompleteNotifyStaffBean.java
+++ maven/QuizMaster/QuizMaster-ejb/src/main/java/edu/depaul/cdm/QuizMaster/mdb/QuizMatchCompleteNotifyStaffBean.java
@@ -5,10 +5,15 @@
  */
 package edu.depaul.cdm.QuizMaster.mdb;
 
+import edu.depaul.cdm.QuizMaster.DTODescriptor.QuizMatchDescriptor;
+import edu.depaul.cdm.QuizMaster.service.QuizBean;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
 
 /**
  *
@@ -23,13 +28,25 @@ import javax.jms.MessageListener;
 })
 public class QuizMatchCompleteNotifyStaffBean implements MessageListener {
     
+    private static final Logger logger = Logger.getLogger(QuizMatchCompleteNotifyStaffBean.class.getName());
+    
     public QuizMatchCompleteNotifyStaffBean() {
     }
     
     @Override
     public void onMessage(Message message) {
         
-        
+        try {
+            
+            ObjectMessage om = (ObjectMessage)message;
+            QuizMatchDescriptor qmd = (QuizMatchDescriptor)om.getObject();
+            
+            //Notify the staff via email
+            logger.log(Level.FINEST, "New QuizMatch complete message received: {0}  QN: {1} PN: {2}", new Object[]{qmd.getId(), qmd.getQuiz().getName(), qmd.getPlayer().getName()});
+            
+        } catch(Exception e) {
+            logger.log(Level.FINEST, "Uh Oh! Couldnt notify the staff {0}", new Object[]{ e.getMessage() });
+        }
         
     }
     
